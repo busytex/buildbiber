15c70d7c73c2b4fd97577cdbf5595afb241763cd
Vadim Kantorov <vadimkantorov@gmail.com>
2020-11-30 18:53:39 +0100

    ...

--- hintfile_native.sh
+++ hintfile_native.sh
@@ -1,3 +1,5 @@
-static_ext='attributes B Cwd'
-#Data/Dumper Devel/Peek Digest/MD5 Digest/SHA Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/scalar PerlIO/via POSIX re SDBM_File Socket Tie/Hash/NamedCapture Time/HiRes Time/Piece Unicode/Normalize"
+static_ext='Cwd'
 dynamic_ext=''
+
+#attributes B 
+#Data/Dumper Devel/Peek Digest/MD5 Digest/SHA Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/scalar PerlIO/via POSIX re SDBM_File Socket Tie/Hash/NamedCapture Time/HiRes Time/Piece Unicode/Normalize"
