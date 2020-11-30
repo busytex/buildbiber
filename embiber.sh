eea28b15c89c18ee6a6e0a9b8a16e7dd0a22b167
Vadim Kantorov <vadimkantorov@gmail.com>
2020-11-30 15:13:58 +0100

    ...

--- embiber.sh
+++ embiber.sh
@@ -12,14 +12,14 @@ PREFIX=$ROOT/build/native/prefix
 
 
 platform='x86_64-linux'
-platform_='x86_64-linux-gnu'
+platformsys='x86_64-linux-gnu'
 perlv='5.30.0'
 
 MODULESSYS="deprecate Pod::Simple::TranscodeSmart Pod::Simple::TranscodeDumb List::MoreUtils::XS List::SomeUtils::XS List::MoreUtils::PP HTTP::Status HTTP::Date Encode:: File::Find::Rule IO::Socket::SSL IO::String PerlIO::utf8_strict Text::CSV_XS DateTime"
 MODULESBIBER="Biber::Input::file::bibtex Biber::Input::file::biblatexml Biber::Output::dot Biber::Output::bbl Biber::Output::bblxml Biber::Output::bibtex Biber::Output::biblatexml"
-LIBSSYSROOT=/usr/lib/${platform_}
+LIBSSYSROOT=/usr/lib/$platformsys
 LIBSSYS="libxml2.so libz.so libxslt.so libexslt.so libssl.so.1.1 libcrypto.so.1.1"
-LIBS="/usr/lib/libbtparse.so.1"
+LIBS="/usr/lib/libbtparse.so"
 FILESBIBER="../data/biber-tool.conf;lib/Biber/biber-tool.conf ../data/schemata/config.rnc;lib/Biber/config.rnc ../data/schemata/config.rng;lib/Biber/config.rng ../data/schemata/bcf.rnc;lib/Biber/bcf.rnc ../data/schemata/bcf.rng;lib/Biber/bcf.rng ../data/bcf.xsl;Biber/bcf.xsl Biber/LaTeX/recode_data.xml;Biber/LaTeX/recode_data.xml"
 FILESUNICODE="$PREFIX/lib/$perlv/Unicode/Collate/Locale;lib/Unicode/Collate/Locale $PREFIX/lib/$perlv/Unicode/Collate/CJK;lib/Unicode/Collate/CJK $PREFIX/lib/$perlv/Unicode/Collate/allkeys.txt;lib/Unicode/Collate/allkeys.txt $PREFIX/lib/$perlv/Unicode/Collate/keys.txt;lib/Unicode/Collate/keys.txt"
 FILESSYS="$PREFIX/lib/site_perl/$perlv/Mozilla/CA/cacert.pem;lib/Mozilla/CA/cacert.pem $PREFIX/lib/$perlv/$platform/PerlIO;lib/PerlIO $PREFIX/lib/$perlv/$platform/auto/PerlIO;lib/auto/PerlIO $PREFIX/lib/site_perl/$perlv/Business/ISBN/RangeMessage.xml;lib/Business/ISBN/RangeMessage.xml"
