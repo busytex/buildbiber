3986867e892b4da4cb7f7350512140ca2d60a6a4
Vadim Kantorov <vadimkantorov@gmail.com>
2020-11-30 17:05:39 +0100

    ...

---
+++ hintfile_.sh
@@ -0,0 +1,1 @@
+static_ext="attributes B Cwd Data/Dumper Devel/Peek Digest/MD5 Digest/SHA Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/scalar PerlIO/via POSIX re SDBM_File Socket Tie/Hash/NamedCapture Time/HiRes Time/Piece Unicode/Normalize"
