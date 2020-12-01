d6b4f233b8d7ba5b1fc501ed39b187e206209b27
Vadim Kantorov <vadimkantorov@gmail.com>
2020-12-02 00:25:41 +0100

    ...

--- emperl.sh
+++ emperl.sh
@@ -1,5 +1,5 @@
 ROOT=$PWD
-export MAKEFLAGS=-j2
+#export MAKEFLAGS=-j2
 URLPERL=https://www.cpan.org/src/5.0/perl-5.30.0.tar.gz
 #source emperl_config.sh
 
@@ -8,11 +8,34 @@ mkdir -p source build/native/perl build/wasm/perl
 wget -nc $URLPERL -P source
 
 tar -xf source/$(basename $URLPERL) --strip-components=1 --directory=build/native/perl
+
+CPANMODULES="Log::Log4perl autovivification URI Business::ISBN Business::ISSN Business::ISMN DateTime::Format::Builder DateTime::Calendar::Julian Sort::Key Text::Roman Data::Dump Data::Compare Data::Uniqid Mozilla::CA Regexp::Common Class::Accessor File::Slurper IO::String  List::AllUtils Encode::Locale"
+# Unicode::LineBreak
+# Parse::RecDescent PerlIO::utf8_strict
+# Unicode::GCString 
+# IPC::Run3 IO::File 
+# Encode::EUCJPASCII Encode::JIS2K Encode::HanExtra 
+# LWP::UserAgent LWP::Protocol::https
+# Lingua::Translit 
+# List::MoreUtils List::MoreUtils::XS
+# Text::BibTeX Text::CSV Text::CSV_XS
+# XML::LibXML XML::LibXML::Simple XML::LibXSLT XML::Writer
+
+for MOD in $CPANMODULES; do
+    URL=$(wget -q -O - https://fastapi.metacpan.org/v1/download_url/$MOD | grep download_url | cut -d'"' -f4)
+    PKG=${MOD//::/-}
+    wget -nc $URL -P source
+    mkdir -p build/native/perl/ext/$PKG
+    tar -xf source/$(basename $URL) --strip-components=1 --directory build/native/perl/ext/$PKG
+    echo $PKG $URL
+done
+
 pushd build/native/perl
-bash ./Configure -sde -Dprefix=$PWD/../prefix
-make miniperl generate_uudmap
-make perl
-make install
+bash ./Configure -sde -Dprefix=$PWD/../prefix -Dhintfile=$PWD/../../../hintfile_native.sh -Dlibs="-lm" # -lcrypto" 
+make
+#make miniperl generate_uudmap
+#make perl
+#make install
 popd
 
 #-Aldflags=-lm
--- hintfile_native.sh
+++ hintfile_native.sh
@@ -1,5 +1,12 @@
-static_ext='Cwd'
-dynamic_ext=''
-
-#attributes B 
-#Data/Dumper Devel/Peek Digest/MD5 Digest/SHA Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/scalar PerlIO/via POSIX re SDBM_File Socket Tie/Hash/NamedCapture Time/HiRes Time/Piece Unicode/Normalize"
+static_ext='attributes B Cwd Data/Dumper Devel/Peek Digest/MD5 Digest/SHA Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/scalar PerlIO/via POSIX re SDBM_File Socket Tie/Hash/NamedCapture Time/HiRes Time/Piece Unicode/Normalize                        Log/Log4perl autovivification URI Business/ISBN Business/ISSN Business/ISMN DateTime/Format/Builder DateTime/Calendar/Julian   Sort/Key Text/Roman Data/Dump Data/Compare Data/Uniqid Storable Mozilla/CA Regexp/Common Class/Accessor IO/String File/Slurper  List/Util List/AllUtils  Unicode/Collate Encode/Locale'
+# Unicode/LineBreak
+# Parse/RecDescent PerlIO/utf8_strict
+# Unicode/GCString
+# IO/File
+# List/MoreUtils List/MoreUtils/XS
+# IPC/Cmd IPC/Run3
+# Encode/EUCJPASCII Encode/HanExtra Encode/JIS2K
+# LWP/UserAgent LWP/Protocol/https
+# Lingua/Translit
+# Text/BibTeX Text/CSV Text/CSV_XS
+# XML/LibXML XML/LibXML/Simple XML/LibXSLT XML/Writer
