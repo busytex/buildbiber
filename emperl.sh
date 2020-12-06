0da476bc6996acccd0ddccc7e38c2d198f910053
Vadim Kantorov <vadimkantorov@gmail.com>
2020-12-06 17:12:15 +0100

    ...

--- emperl.sh
+++ emperl.sh
@@ -1,26 +1,29 @@
+set -e 
+
 ROOT=$PWD
 #export MAKEFLAGS=-j2
 URLPERL=https://www.cpan.org/src/5.0/perl-5.30.0.tar.gz
 #source emperl_config.sh
+# Unicode::LineBreak Unicode::GCString
+# Lingua::Translit
+# Encode::EUCJPASCII Encode::JIS2K Encode::HanExtra
+# Text::CSV Text::CSV_XS
+# PerlIO::utf8_strict
+# IPC::Run3
+# LWP::UserAgent LWP::Protocol::https
+# XML::LibXSLT XML::Writer
+# XML::LibXML::Simple
+
+# List::MoreUtils List::MoreUtils::XS
+# Alien::Base::Wrapper XML::LibXML
+# Text::BibTeX
 
 mkdir -p source build/native/perl build/wasm/perl
 
 wget -nc $URLPERL -P source
 
 tar -xf source/$(basename $URLPERL) --strip-components=1 --directory=build/native/perl
-
-CPANMODULES="Log::Log4perl autovivification URI Business::ISBN Business::ISSN Business::ISMN DateTime::Format::Builder DateTime::Calendar::Julian Sort::Key Text::Roman Data::Dump Data::Compare Data::Uniqid Mozilla::CA Regexp::Common Class::Accessor File::Slurper IO::String  List::AllUtils Encode::Locale Parse::RecDescent"
-# Unicode::LineBreak
-# PerlIO::utf8_strict
-# Unicode::GCString 
-# IPC::Run3 IO::File 
-# Encode::EUCJPASCII Encode::JIS2K Encode::HanExtra 
-# LWP::UserAgent LWP::Protocol::https
-# Lingua::Translit 
-# List::MoreUtils List::MoreUtils::XS
-# Text::BibTeX Text::CSV Text::CSV_XS
-# XML::LibXML XML::LibXML::Simple XML::LibXSLT XML::Writer
-
+CPANMODULES="Log::Log4perl autovivification URI Business::ISBN Business::ISSN Business::ISMN DateTime::Format::Builder DateTime::Calendar::Julian Sort::Key Text::Roman Data::Dump Data::Compare Data::Uniqid Mozilla::CA Regexp::Common Class::Accessor File::Slurper IO::String  List::AllUtils Encode::Locale Parse::RecDescent" 
 for MOD in $CPANMODULES; do
     URL=$(wget -q -O - https://fastapi.metacpan.org/v1/download_url/$MOD | grep download_url | cut -d'"' -f4)
     PKG=${MOD//::/-}
@@ -29,14 +32,48 @@ for MOD in $CPANMODULES; do
     tar -xf source/$(basename $URL) --strip-components=1 --directory build/native/perl/ext/$PKG
     echo $PKG $URL
 done
-
 pushd build/native/perl
-bash ./Configure -sde -Dprefix=$PWD/../prefix -Dhintfile=$PWD/../../../hintfile_native.sh -Dlibs="-lm" # -lcrypto" 
+bash ./Configure -sde -Dprefix=$PWD/../prefix -Dhintfile=$ROOT/hintfile_native.sh -Dlibs="-lm" 
 make
-#make miniperl generate_uudmap
-#make perl
-#make install
+##make miniperl generate_uudmap
+##make install
 popd
+
+PERLBIN=$PWD/build/native/perl/perl
+export PERLLIB=$PWD/build/native/perl/lib:$PWD/build/native/perl/ext/Encode-HanExtra
+export PATH=$PWD/build/native/perl/cpan/Encode/bin/:$PATH
+export INC="-I$PWD/build/native/perl -I$PWD/build/native/perl/lib/Encode -I$PWD/build/native/perl/ext/Unicode-LineBreak/sombok/include/ -I/usr/include/libxml2/"
+mkdir -p build/native/prefix/bin
+rm -f build/native/prefix/bin/enc2xs || true
+cp build/native/perl/cpan/Encode/bin/enc2xs build/native/prefix/bin/enc2xs
+chmod +x build/native/prefix/bin/enc2xs
+CPANMODULES="Encode::EUCJPASCII Encode::JIS2K Encode::HanExtra Lingua::Translit Unicode::LineBreak Unicode::GCString Text::CSV Text::CSV_XS PerlIO::utf8_strict IPC::Run3 LWP::UserAgent LWP::Protocol::https XML::LibXSLT XML::Writer"
+#CPANMODULES="XML::LibXML::Simple"
+# depends on Alien/base/Wrapper.pm "XML::LibXML XML::LibXML::Simple XML::LibXSLT XML::Writer"  
+# no Makefile Text::BibTeX 
+#CPANMODULES="List::MoreUtils::XS"
+# Checking whether perlapi is accessible... no
+# configure: Cannot use Perl API - giving up
+# Checking whether pureperl is required... no
+#Checking for cc... ld: warning: cannot find entry symbol _start; defaulting to 00000000004000b0
+#cc
+#Checking for cc... (cached) cc
+# Alien::Base::Wrapper 
+#"XML::LibXML XML::LibXML::Simple" 
+for MOD in $CPANMODULES; do
+    URL=$(wget -q -O - https://fastapi.metacpan.org/v1/download_url/$MOD | grep download_url | cut -d'"' -f4)
+    PKG=${MOD//::/-}
+    wget -nc $URL -P source
+    rm -rf build/native/perl/ext/$PKG || true
+    mkdir -p build/native/perl/ext/$PKG
+    tar -xf source/$(basename $URL) --strip-components=1 --directory build/native/perl/ext/$PKG
+    echo $PKG $URL
+    
+    pushd build/native/perl/ext/$PKG
+    $PERLBIN Makefile.PL INC="$INC"
+    make INC="$INC"
+    popd
+done
 
 #-Aldflags=-lm
 
--- hintfile_native.sh
+++ hintfile_native.sh
@@ -1,12 +1,12 @@
 static_ext='attributes B Cwd Data/Dumper Devel/Peek Digest/MD5 Digest/SHA Encode Fcntl File/Glob Hash/Util I18N/Langinfo IO List/Util mro Opcode PerlIO/encoding PerlIO/scalar PerlIO/via POSIX re SDBM_File Socket Tie/Hash/NamedCapture Time/HiRes Time/Piece Unicode/Normalize                        Log/Log4perl autovivification URI Business/ISBN Business/ISSN Business/ISMN DateTime/Format/Builder DateTime/Calendar/Julian   Sort/Key Text/Roman Data/Dump Data/Compare Data/Uniqid Storable Mozilla/CA Regexp/Common Class/Accessor IO/String File/Slurper  List/Util List/AllUtils  Unicode/Collate Encode/Locale Parse/RecDescent'
-# Unicode/LineBreak
-#  PerlIO/utf8_strict
-# Unicode/GCString
-# IO/File
-# List/MoreUtils List/MoreUtils/XS
+
+#static_ext='Encode/EUCJPASCII'
 # IPC/Cmd IPC/Run3
-# Encode/EUCJPASCII Encode/HanExtra Encode/JIS2K
+# Unicode/LineBreak Unicode/GCString
+# List/MoreUtils List/MoreUtils/XS IO/File
+# PerlIO/utf8_strict
+#  Encode/HanExtra Encode/JIS2K
 # LWP/UserAgent LWP/Protocol/https
-# Lingua/Translit
+# Lingua/Translit (depends on IO::File)
 # Text/BibTeX Text/CSV Text/CSV_XS
 # XML/LibXML XML/LibXML/Simple XML/LibXSLT XML/Writer
