46c346a74dc1392560aa8f96838de95ae8194e36
Vadim Kantorov <vadimkantorov@gmail.com>
2021-10-01 19:46:59 +0200

    ...

--- emperl.c
+++ emperl.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv, char **env)
        my_perl = perl_alloc();
        perl_construct(my_perl);
        PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
-       perl_parse(my_perl, NULL, argc, argv, (char **)NULL);
+       perl_parse(my_perl, xs_init, argc, argv, (char **)NULL);
        perl_run(my_perl);
        perl_destruct(my_perl);
        perl_free(my_perl);
