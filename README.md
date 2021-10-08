8343b303881e963128527196dbbd4efa4f150823
Vadim Kantorov <vadimkantorov@gmail.com>
2021-10-08 15:40:35 +0200

    ...

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 - https://stackoverflow.com/questions/4855909/perl-including-embedding-a-module-in-a-script
 - https://stackoverflow.com/questions/4158900/embedding-resources-in-executable-using-gcc
 - https://stackoverflow.com/questions/5479691/is-there-any-standard-way-of-embedding-resources-into-linux-executable-image/10692876#10692876
+- https://github.com/Perl/perl5/compare/blead...haukex:emperl_v5.30.0
--- emperl.c
+++ emperl.c
@@ -4,7 +4,6 @@
 
 #include <xsinit.c>
 
-static PerlInterpreter *my_perl;
 static char script[1 << 20];
 
 extern char _binary_fmtutil_pl_start[];
@@ -19,22 +18,24 @@ int main(int argc, char **argv, char **env)
     //fread(script, sizeof(script), 1, f);
     //fclose(f);
     
+    PERL_SYS_INIT3(&argc, &argv, &env);
+    PerlInterpreter* my_perl = perl_alloc();
+    perl_construct(my_perl);
+    PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
+    
+    //perl_parse(my_perl, xs_init, argc, argv, (char **)NULL);
+    
     int iSize =  (int)(_binary_fmtutil_pl_end - _binary_fmtutil_pl_start);
     strncpy(script,    _binary_fmtutil_pl_start, iSize);
     script[iSize] = '\0';
 
-    char *one_args[] = { "one_perl", "-e", script, argv[1], NULL };
-
-    PERL_SYS_INIT3(&argc,&argv,&env);
-    my_perl = perl_alloc();
-    perl_construct(my_perl);
-    PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
-    //perl_parse(my_perl, xs_init, argc, argv, (char **)NULL);
-    perl_parse(my_perl, xs_init, 4, one_args, (char **)NULL);
+    char *one_args[] = { "my_perl", "-e", script, NULL };
+    perl_parse(my_perl, xs_init, 3, one_args, (char **)NULL);
     
     perl_run(my_perl);
     perl_destruct(my_perl);
     perl_free(my_perl);
     PERL_SYS_TERM();
-    exit(EXIT_SUCCESS);
+
+    return 0;
 }
