cb00773f5ba6a76ca95e583fcc3491fe40e6fd7f
Vadim Kantorov <vadimkantorov@gmail.com>
2024-02-07 14:14:27 +0200

    ...

---
+++ pack_perl_modules.py
@@ -0,0 +1,71 @@
+# https://stackoverflow.com/a/69438160/445810
+
+import os
+import sys
+import argparse
+import urllib.request
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--encoding', default = 'utf-8')
+parser.add_argument('--method', default = 'inchook', choices = ['inchook', 'incpatch'], help = 'order of pm files is important for [incpatch] and not important for [inchook]')
+parser.add_argument('--delete-pod', nargs = '*', default = ['=pod,=cut', '__END__,=cut', '=head1,=cut', '=item,=cut'])
+parser.add_argument('--delete-pod-sep', default = ',')
+parser.add_argument('--delete-comments-naive', action = 'store_true')
+parser.add_argument('--pl')
+parser.add_argument('--pm', nargs = '*')
+args = parser.parse_args()
+
+def read_pl_source(p, ):
+    f = urllib.request.urlopen(p) if p.startswith('https://') or p.startswith('http://') else open(p, mode)
+    t = f.read().decode(args.encoding)
+    for be in args.delete_pod:
+        r = ''
+        b, e = be.split(args.delete_pod_sep)
+        skip = False
+        for l in t.splitlines():
+            s = l.split()
+            if s and s[0] == b:
+                skip = True
+            if (skip is False) and (not l or args.delete_comments_naive is False or not l.lstrip().startswith('#')):
+                r += l + '\n'
+            if l == e:
+                skip = False
+        t = r
+    return t
+
+
+if args.method == 'inchook':
+    print('BEGIN {')
+    print('my %modules = (')
+    for p in args.pm:
+        path, *key = p.split('@')
+        if not key:
+            key = [os.path.basename(path)]
+        print(f'''"${key[0]}" => <<'__EOI__',''')
+        print(read_pl_source(path))
+        print('1;')
+        print('__EOI__')
+    print(');')
+    print('unshift @INC, sub {')
+    print('my $module = $modules{$_[1]}')
+    print('or return;')
+    print('return \\$module')
+    print('};')
+    print('}')
+
+if args.method == 'incpatch':
+    for p in args.pm:
+        path, *key = p.split('@')
+        if not key:
+            key = [os.path.basename(path)]
+        print('# PACKPERLMODULESBEGIN', p)
+        print('BEGIN {')
+        print(read_pl_source(path))
+        print('$$INC{ ( __PACKAGE__ =~ s{::}{/}rg ) . ".pm" } = 1;')
+        print('}')
+        print('# PACKPERLMODULESEND', p)
+
+if args.pl:
+    print('# PACKPERLMODULESBEGIN', args.pl)
+    print(read_pl_source(args.pl))
+    print('# PACKPERLMODULESEND', args.pl)
