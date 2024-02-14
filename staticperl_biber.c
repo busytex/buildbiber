1dc442b87266bf0bd3f59545576c635b3310df54
Vadim Kantorov <vadimkantorov@gmail.com>
2024-02-14 23:16:09 +0100

    ...

--- staticperl_biber.c
+++ staticperl_biber.c
@@ -29,8 +29,8 @@ void xs_init         (pTHX)
     dXSUB_SYS;
     PERL_UNUSED_CONTEXT;
 
-    //newXS("Fcntl::bootstrap", boot_Fcntl, file);
-    //newXS("IO::bootstrap", boot_IO, file);
+    newXS("Fcntl::bootstrap", boot_Fcntl, file);
+    newXS("IO::bootstrap", boot_IO, file);
     newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
 }
 ///////////////////////////////////////
@@ -45,16 +45,13 @@ int main(int argc, char **argv)
     perl_construct(my_perl);
     PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
 
-    
     char *one_args[] = { "staticperl_biber", "-e", script, NULL };
-    perl_parse(my_perl, xs_init, 5, one_args, (char **)NULL);
+    perl_parse(my_perl, xs_init, 3, one_args, (char **)NULL);
     
     perl_run(my_perl);
-    /*
     perl_destruct(my_perl);
     perl_free(my_perl);
     PERL_SYS_TERM();
-    */
 
     return 0;
 }
