7a9fd76470a3daf97d24fd375064e9cf76efa245
Vadim Kantorov <vadimkantorov@gmail.com>
2024-02-14 23:06:35 +0100

    ...

--- staticperl_biber.c
+++ staticperl_biber.c
@@ -45,9 +45,10 @@ int main(int argc, char **argv)
     perl_construct(my_perl);
     PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
 
-    /*
+    
     char *one_args[] = { "staticperl_biber", "-e", script, NULL };
     perl_parse(my_perl, xs_init, 5, one_args, (char **)NULL);
+    /*
     perl_run(my_perl);
     perl_destruct(my_perl);
     perl_free(my_perl);
