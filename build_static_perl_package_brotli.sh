38f59cff84fcb22cde69bcb73e968123b8a69d3f
Vadim Kantorov <vadimkantorov@gmail.com>
2024-02-15 20:17:02 +0100

    ...

--- build_static_perl_package_brotli.sh
+++ build_static_perl_package_brotli.sh
@@ -2,7 +2,6 @@ MOD="$1"
 URL=$(wget -q -O - https://fastapi.metacpan.org/v1/download_url/$MOD | grep download_url | cut -d'"' -f4)
 PKG=${MOD//::/-}
 echo $PKG $URL
-
 mkdir -p myextsource myext/$PKG
 wget -nc $URL -P myextsource
 tar -xf myextsource/$(basename $URL) --strip-components=1 --directory myext/$PKG
