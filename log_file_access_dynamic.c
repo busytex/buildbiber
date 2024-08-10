6010c4361c09eafbcd1d1c8c2eaae150c1987567
Vadim Kantorov <vadimkantorov@gmail.com>
2024-08-10 20:17:53 +0200

    ...

--- log_file_access_dynamic.c
+++ log_file_access_dynamic.c
@@ -91,6 +91,7 @@ int fstat(int fd, struct stat *restrict statbuf)
     orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "fstat");
     return orig_func(fd, statbuf);
 }
+
 int fstatat(int dirfd, const char *restrict pathname, struct stat *restrict statbuf, int flags)
 {
     typedef int (*orig_func_type)(int dirfd, const char *restrict pathname, struct stat *restrict statbuf, int flags);
@@ -98,13 +99,15 @@ int fstatat(int dirfd, const char *restrict pathname, struct stat *restrict stat
     orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "fstatat");
     return orig_func(dirfd, pathname, statbuf, flags);
 }
-int fstatat64(int dirfd, const char *restrict pathname, struct stat64 *restrict statbuf, int flags)
+
+/*int fstatat64(int dirfd, const char *restrict pathname, struct stat64 *restrict statbuf, int flags)
 {
     typedef int (*orig_func_type)(int dirfd, const char *restrict pathname, struct stat64 *restrict statbuf, int flags);
     fprintf(stderr, "log_file_access_preload: fstat64(%d, \"%s\", %p, %d)\n", dirfd, pathname, (void*)statbuf, flags);
     orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "fstatat64");
     return orig_func(dirfd, pathname, statbuf, flags);
-}
+}*/
+
 int newfstatat(int dirfd, const char *restrict pathname, struct stat *restrict statbuf, int flags)
 {
     typedef int (*orig_func_type)(int dirfd, const char *restrict pathname, struct stat *restrict statbuf, int flags);
